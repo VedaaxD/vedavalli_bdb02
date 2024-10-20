18a3e7df14e267d31314919e0a77c7d280588389
Vedavalli V <121811820+VedaaxD@users.noreply.github.com>
2024-10-20 14:11:02 +0530

    Delete pythonworksheet2 directory

--- pythonworksheet2/q4.py
+++
@@ -1,7 +0,0 @@
-# Write a program to print the duplicate elements in a list, L.
-L=['10','33','veda','90','8.4','lists','90','python','33','veda']
-duplicate=[i for i in L if L.count(i)>1 ]
-print("The duplicates are",duplicate) #this returns all the repeated elements multiple times
-L=['10','33','veda','90','8.4','lists','90','python','33','veda']
-duplicate=set([ i for i in L if L.count(i)>1 ])
-print("The duplicates are",duplicate) #set returns the duplicate elements only once
